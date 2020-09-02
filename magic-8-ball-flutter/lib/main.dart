a0e99c12c04ba4186e1ccd0adec23f0f41c73aa7
DESKTOP-NV80HKH\aschoi <cas4070@gmail.com>
2020-09-02 22:01:48 +0900

    섹션 8. Boss Level Challenge 1 - Magic 8 Ball
    Step2 - Create a Stateless Widget (Solution)

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -2,22 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue[900],
-          title: Text('Ask Me Anything'),
-        ),
-        body: Container(),
-        backgroundColor: Colors.blue,
-      ),
-    ),
+    MaterialApp(home: BallPage()),
   );
 }
 
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+      ),
+      body: Container(),
+      backgroundColor: Colors.blue,
+    );
   }
 }
