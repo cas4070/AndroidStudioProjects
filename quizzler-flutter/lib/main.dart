d4894bb5c0c5c4af0d877db6f75ec8739524598f
DESKTOP-NV80HKH\aschoi <cas4070@gmail.com>
2020-09-30 14:34:32 +0900

    99강. Checing User Answers : answers 변수를 설정하고, user answer와 맞는지 check (if 문 사용)

--- quizzler-flutter/lib/main.dart
+++ quizzler-flutter/lib/main.dart
@@ -25,14 +25,14 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List scoreKeeper = <Icon>[];
-  List questions = <String>[
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
     'You can lead a cow down stairs but not up stairs.',
     'Approximately one quarter of human bones are in the feet.',
     'A slug\'s blood is green.',
   ];
-
   int questionNumber = 0;
+  List<bool> answers = [false, true, true];
 
   @override
   Widget build(BuildContext context) {
@@ -70,6 +70,12 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = answers[questionNumber];
+                if (correctAnswer == true) {
+                  print('User got it right');
+                } else {
+                  print('User got it wrong');
+                }
                 setState(
                   () {
                     questionNumber = questionNumber + 1;
@@ -92,6 +98,13 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = answers[questionNumber];
+
+                if (correctAnswer == false) {
+                  print('User got it right');
+                } else {
+                  print('User got it wrong');
+                }
                 setState(
                   () {
                     questionNumber = questionNumber + 1;
