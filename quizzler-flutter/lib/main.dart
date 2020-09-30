e16d0a9b0c613ae04ab3080a91ae4efd4041cf97
DESKTOP-NV80HKH\aschoi <cas4070@gmail.com>
2020-09-30 14:17:22 +0900

    98강. Displaying the Questions : List 사용하여! (index error는 해결하지 않음)

--- quizzler-flutter/lib/main.dart
+++ quizzler-flutter/lib/main.dart
@@ -25,24 +25,14 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List scoreKeeper = <Icon>[
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
+  List scoreKeeper = <Icon>[];
+  List questions = <String>[
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
   ];
+
+  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -56,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -82,10 +72,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 setState(
                   () {
-                    scoreKeeper.add(Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ));
+                    questionNumber = questionNumber + 1;
                   },
                 );
               },
@@ -105,7 +92,11 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(
+                  () {
+                    questionNumber = questionNumber + 1;
+                  },
+                );
               },
             ),
           ),
